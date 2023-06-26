5155d4aabee48d4cd9f6ff1d4db82b69c0737c62
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-06-26 19:13:57 +0530

    Update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toogleDrawer(state){
+			if(state === "open"){
+				document.getElementById("drawer").style.display =  "flex";
+			} else {
+				document.getElementById("drawer").style.display =  "none";
+			}
+			
+		}
+		
---
+++ js/singin.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	const usernameValue = username.value.trim();
+	const passwordValue = password.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
